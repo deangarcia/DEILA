9d0056e056fabf28f4a61970151d8e097a790b73
Dean <dean.c.garcia@gmail.com>
2022-07-23 18:01:55 -0700

    Created Backend Models
    Used for entities basically mirrors the attributes also create classes for specific update, create signatures

---
+++ Deila.backend/Models/ArticleDto.cs
@@ -0,0 +1,21 @@
+﻿using deila.backend.Entities;
+
+namespace deila.backend.Models
+{
+
+    public class ArticleBaseDto
+    {
+        public string? Title { get; set; }
+        public int BasisId { get; set; } 
+        public string? Origin { get; set; }
+        public bool? Sentiment { get; set; }
+        public string? Content { get; set; }
+
+    }
+    public class ArticleDto : ArticleBaseDto
+    {
+        public int Id { get; set; }
+    }
+    public class ArticleCreateDto : ArticleBaseDto { }
+    public class ArticleUpdateDto : ArticleBaseDto { }
+}
---
+++ Deila.backend/Models/BasisDto.cs
@@ -0,0 +1,15 @@
+﻿namespace deila.backend.Models
+{
+    public class BasisBaseDto
+    {
+        public string Category { get; set; } = string.Empty;
+        public int Incidents { get; set; } = 0;
+
+    }
+    public class BasisDto : BasisBaseDto
+    {
+        public int Id { get; set; }
+    }
+    public class BasisCreateDto : BasisBaseDto { }
+    public class BasisUpdateDto : BasisBaseDto { }
+}
