a9258b2399b784dfd171111e0130ac6baf138068
Dean <dean.c.garcia@gmail.com>
2022-07-27 18:26:15 -0700

    Database Seed
    Even though the code first is there and I pointed to the context in the connection string I still had to run Update-Database migration command these were some autogenerated pieces of code.

--- Deila.backend/Migrations/20220724003312_fixederror.cs
+++ Deila.backend/Migrations/20220724003312_fixederror.cs
@@ -8,16 +8,7 @@ namespace deila.backend.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.InsertData(
-                table: "Basiss",
-                columns: new[] { "Id", "Category", "Incidents" },
-                values: new object[,]
-                {
-                    { 1, "Gender", 0 },
-                    { 2, "Race", 0 },
-                    { 3, "Disability", 0 },
-                    { 4, "Other", 0 }
-                });
+           
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
---
+++ Deila.backend/Migrations/20220727224425_CloudDeploy.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace deila.backend.Migrations
+{
+    public partial class CloudDeploy : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
