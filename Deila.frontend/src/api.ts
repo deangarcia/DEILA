541de8be4d0f265c96b1a5aef1ea5545399e1a2d
Dean <dean.c.garcia@gmail.com>
2022-08-12 13:07:14 -0700

    updated for production

--- Deila.frontend/src/api.ts
+++ Deila.frontend/src/api.ts
@@ -28,6 +28,8 @@ export const api = {
     },
     ////////////////////////////////// Sentiment Analysis //////////////////////////////////////////////
     async getSentiment(text: string) {
-        return axios.get<ISentimentAnalysis>(`http://127.0.0.1:8000/api/v1/sentiment_analysis/?text=${text}`);
+        return axios.get<ISentimentAnalysis>(`https://deilasentimentbackend.azurewebsites.net/api/v1/sentiment_analysis/?text=${text}`);
+        //Development
+        //return axios.get<ISentimentAnalysis>(`http://127.0.0.1:8000/api/v1/sentiment_analysis/?text=${text}`);
     },
 };
