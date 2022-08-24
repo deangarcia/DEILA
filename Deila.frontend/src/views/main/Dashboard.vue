69769d2bd89e5fd85a075f0f1320d8f314c0aaa4
Dean <dean.c.garcia@gmail.com>
2022-08-23 20:24:01 -0700

    Update Dashboard.vue

--- Deila.frontend/src/views/main/Dashboard.vue
+++ Deila.frontend/src/views/main/Dashboard.vue
@@ -1,8 +1,16 @@
 <template>
-    <div>
-
-    </div>
+    <v-container fluid>
+        <v-card class="ma-3 pa-3">
+            <v-card-title primary-title>
+                <div class="headline primary--text">Project Mission</div>
+            </v-card-title>
+            <v-card-text>
+                <div class="headline font-weight-light ma-5">To develop a web application that can assist in the effort of creating an inclusive workplace environment. The web application will have two main features: The first feature is an inclusive language sentiment analyzer. And the second feature is a data acquisition and labeling interface. These features go hand in hand, as the data acquisition and labeling tool will contribute to the development of a more robust dataset for training the model in the first feature.</div>
+            </v-card-text>
+        </v-card>
+    </v-container>
 </template>
+
 <script lang="ts">
     import { Component, Vue } from "vue-property-decorator";
 
