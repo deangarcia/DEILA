ed0a4888897b64f6bff0f4b2e730c89f4bbc5fdc
Dean <dean.c.garcia@gmail.com>
2022-08-25 09:21:15 -0700

    adding to npm serve domain to cors
    need to try and add footer for github

---
+++ Deila.frontend/src/components/Footer.vue
@@ -0,0 +1,27 @@
+<template>
+        <v-footer dark
+                  padless fixed app>
+            <v-card class="flex"
+                    flat
+                    tile>
+                <v-card-text class="pa-0 white--text text-center grey text--secondary">
+                        Controlled by: NSWC Corona Division, CUI Category: General Proprietary Business Information, Limited Distribution Control: FEDCON
+
+                        <br> POC: Dean C Garcia (951) 768 - 2194
+                </v-card-text>
+
+                <v-card-text class="pa-0 white--text text-center purple darken-4">CUI
+                </v-card-text>
+            </v-card>
+        </v-footer>
+</template>
+
+<script lang="ts">
+    import { Vue, Component } from 'vue-property-decorator';
+    import '@/assets/sram.css';
+
+    @Component
+    export default class Footer extends Vue {
+        
+    }
+</script>
